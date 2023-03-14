7fc024f37226ea05daebac46b89990f27eacbac8
PanHH <panxq0809@163.com>
2023-03-14 17:35:53 +0800

    Updates

---
+++ precache-manifest.43c390d7f2f8f4213fdd418b53a4de6d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "72e70987e677a481ef90",
+    "url": "/walletconnect-dapp178/static/js/main.72e70987.chunk.js"
+  },
+  {
+    "revision": "0973dad8f157d017b828",
+    "url": "/walletconnect-dapp178/static/js/1.0973dad8.chunk.js"
+  },
+  {
+    "revision": "307a7cb177506c8a77d3",
+    "url": "/walletconnect-dapp178/static/js/runtime~main.307a7cb1.js"
+  },
+  {
+    "revision": "e1cb8d21bffbcc2a8c7bfcf95bad12ff",
+    "url": "/walletconnect-dapp178/static/media/walletconnect-banner.e1cb8d21.png"
+  },
+  {
+    "revision": "906078994f7221a25b900f47425b3ecc",
+    "url": "/walletconnect-dapp178/static/media/erc20.90607899.svg"
+  },
+  {
+    "revision": "42bca7317690c3f5e9ecd8db387344df",
+    "url": "/walletconnect-dapp178/static/media/eth.42bca731.svg"
+  },
+  {
+    "revision": "e37299a879ceb5a23000d8a2bb10bc46",
+    "url": "/walletconnect-dapp178/static/media/xdai.e37299a8.png"
+  },
+  {
+    "revision": "93e374d6dd275d888c63b674f0511d2b",
+    "url": "/walletconnect-dapp178/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,35 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("/walletconnect-dapp178/workbox-v3.6.3/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "/walletconnect-dapp178/workbox-v3.6.3"});
+
+importScripts(
+  "/walletconnect-dapp178/precache-manifest.43c390d7f2f8f4213fdd418b53a4de6d.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/walletconnect-dapp178/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
